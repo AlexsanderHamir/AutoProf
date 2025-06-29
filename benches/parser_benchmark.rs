982befe29b8f1f6e030c5374502fec2cc318428e
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-06-29 06:53:02 -0700

    Benchmarks

---
+++ benches/parser_benchmark.rs
@@ -0,0 +1,19 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use gocortex::parser::profile_parsing::extract_profile_data;
+use std::path::PathBuf;
+
+pub fn parser_benchmark(c: &mut Criterion) {
+    let profile_file_path = PathBuf::from("tag_name_example/text/BenchmarkGenPool/BenchmarkGenPool_cpu.txt");
+    c.bench_function("parse_profile_data", |b| {
+        b.iter(|| match extract_profile_data(&profile_file_path) {
+            Ok((_header, _functions_profile_data)) => {}
+            Err(e) => {
+                eprintln!("Error parsing profile data: {}", e);
+                std::process::exit(1);
+            }
+        })
+    });
+}
+
+criterion_group!(benches, parser_benchmark);
+criterion_main!(benches);
