a39e20e0e81fed4a7496dd5483ac88bcb0554e17
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-07-02 10:26:40 -0700

    Module Deleted
    1. There's no reason to have a module of a single function, the parser performs the parsin and the token reduction.
    2. Tests and benchmarks need to be updated because of this changes.

--- main.rs
+++ main.rs
@@ -1,6 +1,5 @@
 use std::{fs, path::PathBuf};
 mod parser;
-mod rewriter;
 use tiktoken_rs::o200k_base;
 
 fn main() {
@@ -8,8 +7,8 @@ fn main() {
     let bpe_encoder = o200k_base().unwrap();
 
     match parser::interface::parse_profile_data(&profile_file_path) {
-        Ok((header_string, functions_profile_data)) => {
-            let rewritten_profile_data = rewriter::interface::rewrite_profile_data(header_string, functions_profile_data);
+        Ok(rewritten_profile_data) => {
+            println!("{}", rewritten_profile_data);
             let tokens = bpe_encoder.encode_with_special_tokens(&rewritten_profile_data);
             println!("Optimized Parsing: {}", tokens.len());
         }
